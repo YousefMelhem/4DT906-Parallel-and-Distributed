f4b1f5a1bb8e97de0e7fae08881584f8e5b5a9c1
km222uq <km222uq@student.lnu.se>
2025-02-21 11:30:48 +0100

    started some logging

---
+++ Assignment1/plot.py
@@ -0,0 +1,8 @@
+
+
+metrics = ["Name", "GFlop/s", "block_size", "Matrix Size", "Threads" "Other"]
+
+logs = [
+    ["Python", "0.014171", "0", "256", "1"],
+    ["Numpy", "29.757932", "0", "1024", "1"],
+]
