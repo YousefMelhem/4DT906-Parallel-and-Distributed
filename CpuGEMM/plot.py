0265b475d3848f35c0ffaaeab8377a4bf04eadef
km222uq <km222uq@student.lnu.se>
2025-03-08 13:51:42 +0100

    some updates

--- CpuGEMM/plot.py
+++ CpuGEMM/plot.py
@@ -3,26 +3,29 @@ from time import monotonic
 import matplotlib.pyplot as plt
 
 
-cpp_flops = [171.834152, 395.073334, 393.556854, 266.474579, 222.968567]
-np_flops = []
-n = [512, 1024, 2048, 4096, 8192]
-
-
-for i in n:
-    A = np.random.random((i, i)).astype(np.float32)
-    B = np.random.random((i, i)).astype(np.float32)
+def calcnpflops(n):
+    A = np.random.random((n, n)).astype(np.float32)
+    B = np.random.random((n, n)).astype(np.float32)
     start = monotonic()
     C = A @ B
     end = monotonic()
     s = end - start
 
-    gflops = (i*i*2*i) / (s * 10**9)
-    np_flops.append(gflops)
+    gflops = (n*n*2*n) / (s * 10**9)
+    return gflops
 
 
-plt.plot(n, cpp_flops, label="C++")
+cpp_flops = [171.834152, 395.073334, 393.556854, 266.474579, 222.968567]
+accelerate = [263.948334, 887.389954,
+              1248.083496, 1293.433472, 1398.854248]
 
-plt.plot(n, np_flops, label="Numpy")
+N = [512, 1024, 2048, 4096, 8192]
+
+np_flops = [calcnpflops(i) for i in N]
+
+plt.plot(N, cpp_flops, label="C++")
+plt.plot(N, np_flops, label="Numpy")
+plt.plot(N, accelerate, label="Accelerate")
 
 plt.xlabel("N")
 
